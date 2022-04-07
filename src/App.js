ab9efc3b10ae5c15bf5e1d6c36ac081f3f25226c
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-04-07 14:56:00 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+
+    return (
+      <div className="App">
+          <h1>Hello</h1>
+          <button>
+              Hello 2
+          </button>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+)
