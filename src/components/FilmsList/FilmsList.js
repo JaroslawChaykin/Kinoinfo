086837248061eda07bcdf212600db5dd493889bb
DenisRaymer <raymerdenis@gmail.com>
2022-05-07 12:20:37 +0300

    ref: Refactoring

--- src/components/FilmsList/FilmsList.js
+++ src/components/FilmsList/FilmsList.js
@@ -1,3 +1,4 @@
+import React, {Fragment} from 'react'
 import FilmItem from '../FilmItem/FilmItem';
 import Loader from '../UI/Loader/Loader';
 import { moviesAPI } from '../../services/MoviesService';
@@ -5,24 +6,18 @@ import classes from './FilmsList.module.scss';
 
 const FilmsList = () => {
     const {data: movies, isLoading, error} = moviesAPI.useFetchMoviesQuery('');
+    
+    if (isLoading) return <Loader/>
+        
+    if (error) return <h1>{error}</h1>
+    
+    if (movies.items.length === 0) return <h1>Список фильмов пуст</h1>
+    
     return (
-      <>
-          {error && <h1>{error}</h1>}
-          {isLoading
-            ? <Loader/>
-            :
-            <>
-                {
-                    movies.items.length === 0
-                      ?
-                      <h1>Список фильмов пуст</h1>
-                      :
-                      <div className={classes.filmsList}>
-                          {movies.items.map((movie, index) => <FilmItem key={movie.kinopoiskId + index} film={movie}/>)}
-                      </div>
-                }
-            </>
-          }
+      <Fragment>
+        <div className={classes.filmsList}>
+          {movies.items.map((movie, index) => <FilmItem key={movie.kinopoiskId + index} film={movie}/>)}
+        </div>
       </>
     );
 };
